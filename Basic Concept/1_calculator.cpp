6ae5ca06acb6211edb71482dd08ad392384655de
Rachna Pandya <ruchipandya20@gmail.comgit config --global user.email ruchipandya20@gmail.com>
2024-09-03 23:25:35 +0530

    first commit

---
+++ Basic Concept/1_calculator.cpp
@@ -0,0 +1,44 @@
+// WAP to create simple calculator using class
+
+#include <iostream>
+using namespace std;
+
+class calculator
+{
+public:
+    int sum(int a, int b, char op)
+    {
+        switch (op)
+        {
+        case '+':
+            return a + b;
+        case '-':
+            return a - b;
+        case '*':
+            return a * b;
+        case '/':
+            return a / b;
+        default:
+            cout << "invalid op";
+        }
+    }
+};
+int main()
+{
+    calculator cal;
+    int a, b;
+    char op;
+
+    cout << "Enter first number: ";
+    cin >> a;
+
+    cout << "Enter second number: ";
+    cin >> b;
+
+    cout << "Enter op: ";
+    cin >> op;
+
+    cout << "result " << cal.sum(a, b, op) << endl;
+
+    return 0;
+}
