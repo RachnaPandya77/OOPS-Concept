c52944137c205aafbc26c1d618c9a2596171eec2
Rachna Pandya <ruchipandya20@gmail.comgit config --global user.email ruchipandya20@gmail.com>
2024-09-22 00:59:24 +0530

    add new file

--- Concept2/3_car.cpp
+++ Concept2/3_car.cpp
@@ -5,8 +5,68 @@ functions to get and set these variables.*/
 #include <iostream>
 using namespace std;
 
+class car
+{
+private:
+    string company;
+    string model;
+    int year;
+
+public:
+    car(string company, string model, int year)
+    {
+        this->company = company;
+        this->model = model;
+        this->year = year;
+    }
+
+    string getcompany()
+    {
+        return company;
+    }
+
+    void setcompany(string company)
+    {
+        this->company = company;
+    }
+
+    string getmodel()
+    {
+        return model;
+    }
+
+    void setmodel(string model)
+    {
+        this->model = model;
+    }
+
+    int getyear()
+    {
+        return year;
+    }
+
+    void setyear(int year)
+    {
+        this->year = year;
+    }
+};
+
 int main()
 {
+    car mycar("TATA", "Naxon", 2024);
+
+    cout << "Company Name: " << mycar.getcompany() << endl;
+    cout << "Model: " << mycar.getmodel() << endl;
+    cout << "Year: " << mycar.getyear() << endl;
+    cout << endl;
+
+    mycar.setcompany("volkswagen");
+    mycar.setmodel("Golf R");
+    mycar.setyear(2023);
+
+    cout << "Updated Company Name: " << mycar.getcompany() << endl;
+    cout << "Updated Model: " << mycar.getmodel() << endl;
+    cout << "Updated Year: " << mycar.getyear() << endl;
 
     return 0;
 }
---
+++ Concept2/4_bank.cpp
@@ -0,0 +1,50 @@
+/*Write C++ program to implement a class called Bank Account that has
+private member variables for account number and balance. Include
+member functions to deposit and withdraw money from the account.*/
+
+#include <iostream>
+using namespace std;
+
+class BankAccount
+{
+private:
+    double ac_num;
+    double ac_balance;
+
+public:
+    BankAccount(int number)
+    {
+        ac_num = number;
+        ac_balance = 0.0;
+    }
+
+    // function
+    void deposit(double amount)
+    {
+        ac_balance += amount;
+        cout << "Deposited " << amount << " to account " << ac_num << " New balance: " << ac_balance << endl;
+    }
+
+    void withdraw(double amount)
+    {
+        ac_balance -= amount;
+        cout << "Withdraw " << amount << " from account " << ac_num << " New balance: " << ac_balance << endl;
+    }
+
+    double getac_balance()
+    {
+        return ac_balance;
+    }
+};
+
+int main()
+{
+    BankAccount myaccount(102030);
+
+    myaccount.deposit(25000);
+    myaccount.withdraw(5000);
+
+    cout << "Final Balance: " << myaccount.getac_balance() << endl;
+
+    return 0;
+}
