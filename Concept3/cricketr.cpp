ddb230bfe5c0ba93032e06dc09f965c71640e9bf
Rachna Pandya <ruchipandya20@gmail.comgit config --global user.email ruchipandya20@gmail.com>
2024-09-25 00:37:00 +0530

    Add new file

--- Concept3/cricketr.cpp
+++ Concept3/cricketr.cpp
@@ -2,3 +2,62 @@
 cricketer. Data member of batsman. Total runs, Average runs and best
 performance. Member functions input data, calculate average runs, Display
 data. (Single Inheritance)*/
+
+#include <iostream>
+using namespace std;
+
+class cricketer
+{
+public:
+    void inputdata()
+    {
+    }
+    void displaydata()
+    {
+        cout << "Cricketer Name: Virat Kohli" << endl;
+    }
+};
+
+class batsman : public cricketer
+{
+public:
+    int totalrun;
+    int averagerun;
+    int bestperformance;
+
+    void inputdata()
+    {
+        cricketer::inputdata();
+        cout << "Enter Total Run ";
+        cin >> totalrun;
+
+        cout << "Enter Average Run ";
+        cin >> averagerun;
+
+        cout << "Enter Best Performance ";
+        cin >> bestperformance;
+    }
+
+    void calculateaverage()
+    {
+    }
+
+    void displaydata()
+    {
+        cricketer::displaydata();
+        cout << "Total Run: " << totalrun << endl;
+        cout << "Average Run: " << averagerun << endl;
+        cout << "Best Performance: " << bestperformance << endl;
+    }
+};
+
+int main()
+{
+    batsman bat;
+    bat.inputdata();
+    bat.calculateaverage();
+
+    bat.displaydata();
+
+    return 0;
+}
