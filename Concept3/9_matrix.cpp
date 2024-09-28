4b59ac8cb7187e9b0b7b15f1f66d7c4d10507ee7
Rachna Pandya <ruchipandya20@gmail.comgit config --global user.email ruchipandya20@gmail.com>
2024-09-29 01:21:59 +0530

    add new file

---
+++ Concept3/9_matrix.cpp
@@ -0,0 +1,69 @@
+// Write a Program of Two 1D Matrix Addition using Operator Overloading
+
+#include <iostream>
+using namespace std;
+
+class matrix
+{
+public:
+    int *array;
+    int size;
+
+    matrix(int n)
+    {
+        size = n;
+        array = new int[size];
+    }
+
+    matrix operator+(matrix &a)
+    {
+        matrix result(size);
+        for (int i = 0; i < size; i++)
+        {
+            result.array[i] + a.array[i];
+        }
+        return result;
+    }
+    void print()
+    {
+        for (int i = 0; i < size; i++)
+        {
+            cout << array[i] << " ";
+        }
+        cout << endl;
+    }
+};
+
+int main()
+{
+    int n;
+    cout << "Size of matrix: ";
+    cin >> n;
+
+    matrix A(n), B(n);
+
+    cout << "Element for A: ";
+    for (int i = 0; i < n; i++)
+    {
+        cin >> A.array[i];
+    }
+
+    cout << "Element for B: ";
+    for (int i = 0; i < n; i++)
+    {
+        cin >> B.array[i];
+    }
+
+    matrix C = A + B;
+
+    cout << "Matrix A: ";
+    A.print();
+
+    cout << "Matrix B: ";
+    B.print();
+
+    cout << "Matrix C (A + B): ";
+    C.print();
+
+    return 0;
+}
